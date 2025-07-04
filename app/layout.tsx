d5d3f2f914cca721b50cb42aca7463e456e88c81
sato <sato0811@outlook.com>
2025-07-04 22:07:15 +0900

    ファビコンファイル名をcamel-favicon.icoに変更しキャッシュ回避

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja">
       <head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/favicon-16x16.png" />
-        {/* PNGアイコンも使いたい場合は下も追加 */}
-        {/* <link rel="icon" type="image/png" href="/icon.png" /> */}
+        <link rel="icon" href="/camel-favicon.ico" />
       </head>
       <body className="bg-black text-white font-sans">
         {/* <ThemeToggle /> */}
